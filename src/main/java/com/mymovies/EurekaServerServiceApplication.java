e3a50a4a07bf3df9483dccab2a47b86101b6d0f5
Ranushan Rachu <ranusha.n@hotmail.fr>
2020-04-26 11:20:59 +0200

    [ORIGIN][MASTER][UPDATED] Add logging

--- src/main/java/com/mymovies/EurekaServerServiceApplication.java
+++ src/main/java/com/mymovies/EurekaServerServiceApplication.java
@@ -1,5 +1,7 @@
 package com.mymovies;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,7 +10,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerServiceApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(EurekaServerServiceApplication.class);
+
 	public static void main(String[] args) {
+		LOGGER.info("Start Service Eureka Server");
 		SpringApplication.run(EurekaServerServiceApplication.class, args);
 	}
 
