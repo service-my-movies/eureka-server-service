32daf00dd087d7e3b85ca4b2b544d1f3116207ca
Ranushan Rachu <ranusha.n@hotmail.fr>
2020-04-30 16:33:51 +0200

    [ORIGIN][MASTER][UPDATED] add refresh scope

--- src/main/java/com/mymovies/EurekaServerServiceApplication.java
+++ src/main/java/com/mymovies/EurekaServerServiceApplication.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RefreshScope
 public class EurekaServerServiceApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EurekaServerServiceApplication.class);
